5113c0bdbe722f3c6eda5a47e4d6e87537ff3bb2
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2025-08-25 21:13:13 +0300

    feat: created thread pool functions

---
+++ include/tpool.h
@@ -0,0 +1,29 @@
+#ifndef TPOOL_H
+#define TPOOL_H
+#include <pthread.h>
+
+#define MAX_TASKS 1024
+
+typedef struct {
+    void (*function)(int);
+    int arg;
+} Task;
+
+typedef struct {
+    Task tasks[MAX_TASKS];
+    int count;
+    pthread_mutex_t lock;
+    pthread_cond_t not_empty;
+} TaskQueue;
+
+typedef struct {
+    pthread_t *threads;
+    int thread_count;
+    TaskQueue queue;
+} ThreadPool;
+
+ThreadPool* threadpool_create(int num_threads);
+void thread_queue_init(TaskQueue *queue); 
+void thread_queue_push(TaskQueue *queue,Task task);
+
+#endif
---
+++ src/tpool.c
@@ -0,0 +1,71 @@
+#include "tpool.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void thread_queue_push(TaskQueue* queue, Task task);
+static void* thread_do_work(void* arg);
+
+void thread_queue_init(TaskQueue* queue) {
+    queue->count = 0;
+    pthread_mutex_init(&queue->lock, NULL);
+    pthread_cond_init(&queue->not_empty, NULL);
+}
+
+ThreadPool* threadpool_create(int num_threads){
+    ThreadPool* tp = malloc(sizeof(ThreadPool));
+    if (!tp) return NULL;
+
+    tp->thread_count = num_threads;
+
+    thread_queue_init(&tp->queue);
+    
+    tp->threads = malloc(sizeof(pthread_t) * num_threads);
+    if (!tp->threads) {
+        free(tp);
+        return NULL;
+    }
+
+    for(int i = 0;i < num_threads;i++){
+        if(pthread_create(&tp->threads[i], NULL, &thread_do_work, tp) != 0){
+            perror("Failed to create the thread");
+            free(tp->threads);
+            free(tp);
+            return NULL;
+        }
+    }
+
+    return tp;
+}
+
+static void* thread_do_work(void* arg) {
+    ThreadPool* tp = (ThreadPool*)arg;
+
+    while (1) {
+        Task task;
+        pthread_mutex_lock(&tp->queue.lock);
+
+        while (tp->queue.count == 0) {
+            pthread_cond_wait(&tp->queue.not_empty, &tp->queue.lock);
+        }
+
+        task = tp->queue.tasks[0];
+        for (int i = 0; i < tp->queue.count - 1; i++) {
+            tp->queue.tasks[i] = tp->queue.tasks[i + 1];
+        }
+        tp->queue.count--;
+        pthread_mutex_unlock(&tp->queue.lock);
+
+        task.function(task.arg);
+    }
+}
+
+void thread_queue_push(TaskQueue* queue, Task task) {
+    pthread_mutex_lock(&queue->lock);
+    queue->tasks[queue->count] = task;
+    queue->count++;
+    pthread_mutex_unlock(&queue->lock);
+    pthread_cond_signal(&queue->not_empty);
+}
+
+
+
